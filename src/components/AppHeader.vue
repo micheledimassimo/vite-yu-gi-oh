63ce32dcd286fb1d1d68b0d8c1265d3d2a87bf17
Michele Di Massimo <167227714+micheledimassimo@users.noreply.github.com>
2024-09-12 16:57:38 +0200

    Initial commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  data() {
+    return { 
+      message: 'Template Vite + Vue'
+    }
+  }
+}
+</script>
+
+<template>
+  <header>
+    <h1>
+      {{ message }}
+    </h1>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/partials/variables' as *;
+
+header {
+  background-color: $mainBgColor;
+  text-align: center;
+  padding: 20px 0;
+}
+
+h1 {
+  color: $mainColor;
+}
+</style>
